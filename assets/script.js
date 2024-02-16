bbacb8c28dfcbc1754c2734073f384beabb92e1c
peterhohk <peterhohk0409@gmail.com>
2024-02-16 18:02:21 +0800

    add dropdown for school division

--- assets/script.js
+++ assets/script.js
@@ -2,47 +2,55 @@
 
 (() => {
 
-  // retrieve saved name
-
-  let name = sessionStorage.getItem("name") || "";
-  $("#input-name").val(name);
-
   // initialise wheel
 
   function initWheel() {
+    if (!$("#input-division").val()) return;
+
+    const name = $("#input-name").val();
+    const division = $("#input-division").val()
+
     $("h1").addClass("in-game");
     $("#input-wrapper").addClass("d-none");
     $("#wheel-wrapper").removeClass("d-none");
 
-    name = $("#input-name").val();
-    sessionStorage.setItem("name", name);
+    let wheelItems;
+    if (division === "CWB") {
+      wheelItems = [
+        { label: "HKD 1,000", weight: 30 },
+        { label: "HKD 500", weight: 14 },
+        { label: "HKD 200", weight: 14 },
+        { label: "HKD 100", weight: 14 },
+        { label: "圖書", weight: 14 },
+        { label: "文具", weight: 14 },
+      ];
+    } else {
+      wheelItems = [
+        { label: "HKD 1,000", weight: 10 },
+        { label: "HKD 500", weight: 18 },
+        { label: "HKD 200", weight: 18 },
+        { label: "HKD 100", weight: 18 },
+        { label: "圖書", weight: 18 },
+        { label: "文具", weight: 18 },
+      ];
+    }
 
-    const wheelItems = [
-      { label: "電影門票兩張" },
-      { label: "HKD 1,000" },
-      { label: "HKD 500" },
-      { label: "HKD 200" },
-      { label: "圖書" },
-      { label: "文具" },
-    ];
     const wheel = new spinWheel.Wheel(document.querySelector("#wheel-wrapper"), {
       items: wheelItems,
       itemLabelColors: ["#664620"],
       itemLabelStrokeColor: "#664620",
       itemLabelStrokeWidth: 0.5,
-      itemLabelRotation: 90,
-      itemLabelAlign: "center",
-      itemLabelRadius: 0.6,
-      itemLabelRadiusMax: 0,
+      itemLabelRadius: 0.8,
+      itemLabelRadiusMax: 0.2,
       itemBackgroundColors: ["#eb3f1f", "#ffffff"],
       lineWidth: 0,
       overlayImage: "assets/images/wheel-overlay.svg",
-      rotation: -180 / wheelItems.length,
       rotationSpeedMax: 600,
       rotationResistance: -60,
       onRest: function (ev) {
         $("#result-modal .modal-title").text(`Congratulations${name ? ", " + name : ""}!`);
         $("#result-modal .prize-message").text(wheelItems[ev.currentIndex].label);
+        $("#result-modal .division-indicator").text(division);
         $("#result-modal").modal("show");
       },
     });
